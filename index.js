b23865accfd0e7d0111b9b3d8f91a592a9a40315
Yaping Liu <yaping.liu12@outlook.com>
2021-03-06 15:41:17 -0800

    finished task 5

--- index.js
+++ index.js
@@ -270,9 +270,12 @@ console.log(get20s(artists));
  
  For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/){
-   /*Your Code Here*/
+function removeArtist(array,indexNum){
+   const newArray=[...array];
+   newArray.splice(indexNum,1);
+   return newArray.length;
 }
+console.log(artists,0);
    
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
